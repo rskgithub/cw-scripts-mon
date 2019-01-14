c9c135d51d905282f4d57676e15a41e0eff31aed
Zhuoyun Wei <wzyboy@wzyboy.org>
2019-01-14 01:57:04 -0500

    Integrate USAGE into ArgumentParser

--- src/mon-put-instance-data.py
+++ src/mon-put-instance-data.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+'''
+This script was inspired by aws-scripts-mon:
+http://docs.amazonwebservices.com/AmazonCloudWatch/latest/DeveloperGuide/mon-scripts-perl.html
+
+Use '--help' to see full usage.
+
+Examples
+
+ To perform a simple test run without posting data to Amazon CloudWatch
+
+  ./mon-put-instance-data.py --mem-util --verify --verbose
+
+ To set a five-minute cron schedule to report memory and disk space utilization to CloudWatch
+
+  */5 * * * * ~/cw-scripts-mon/mon-put-instance-data.py --mem-util --disk-space-util --disk-path=/ --from-cron
+
+  A complementary systemd.timer unit is provided in contrib/ directory, if you do not want to use cron.
+'''
+
 import re
 import argparse
 import subprocess
@@ -13,60 +32,6 @@ try:
 except ModuleNotFoundError:
     HAS_BOTO = False
 
-
-USAGE = '''\
-Usage: mon-put-instance-data.py [options]
-
-  Collects memory, swap, and disk space utilization on a cloud-init
-  instance and sends this data as custom metrics to Amazon CloudWatch.
-
-Description of available options:
-
-  --mem-util          Reports memory utilization in percentages.
-  --mem-used          Reports memory used in megabytes.
-  --mem-avail         Reports available memory in megabytes.
-  --swap-util         Reports swap utilization in percentages.
-  --swap-used         Reports allocated swap space in megabytes.
-  --disk-path=PATH    Selects the disk by the path on which to report.
-  --disk-space-util   Reports disk space utilization in percentages.
-  --disk-space-used   Reports allocated disk space in gigabytes.
-  --disk-space-avail  Reports available disk space in gigabytes.
-
-  --aggregated[=only]    Adds aggregated metrics for instance type, AMI id, and region.
-                         If =only is specified, does not report individual instance metrics
-  --auto-scaling[=only]  Reports Auto Scaling metrics in addition to instance metrics.
-                         If =only is specified, does not report individual instance metrics
-
-  --mem-used-incl-cache-buff  Count memory that is cached and in buffers as used.
-  --memory-units=UNITS        Specifies units for memory metrics.
-  --disk-space-units=UNITS    Specifies units for disk space metrics.
-
-    Supported UNITS are bytes, kilobytes, megabytes, and gigabytes.
-
-  --aws-credential-file=PATH  Specifies the location of the file with AWS credentials.
-  --aws-access-key-id=VALUE   Specifies the AWS access key ID to use to identify the caller.
-  --aws-secret-key=VALUE      Specifies the AWS secret key to use to sign the request.
-  --aws-iam-role=VALUE        Specifies the IAM role used to provide AWS credentials.
-
-  --from-cron  Specifies that this script is running from cron.
-  --verify     Checks configuration and prepares a remote call.
-  --verbose    Displays details of what the script is doing.
-  --version    Displays the version number.
-  --help       Displays detailed usage information.
-
-Examples
-
- To perform a simple test run without posting data to Amazon CloudWatch
-
-  ./mon-put-instance-data.py --mem-util --verify --verbose
-
- To set a five-minute cron schedule to report memory and disk space utilization to CloudWatch
-
-  */5 * * * * ~/aws-scripts-mon/mon-put-instance-data.py --mem-util --disk-space-util --disk-path=/ --from-cron
-
-This script was inspired by aws-scripts-mon:
-http://docs.amazonwebservices.com/AmazonCloudWatch/latest/DeveloperGuide/mon-scripts-perl.html
-'''
 
 KILO = 1024
 MEGA = KILO * 1024
@@ -97,29 +62,69 @@ EC2 = is_running_on_ec2()
 
 def parse_args():
     ap = argparse.ArgumentParser()
-    #ap.add_argument('--help', dest='show_help', action='store_true')
-    #ap.add_argument('--version', dest='show_version', action='store_true')
-    ap.add_argument('--mem-util', dest='report_mem_util', action='store_true')
-    ap.add_argument('--mem-used', dest='report_mem_used', action='store_true')
-    ap.add_argument('--mem-avail', dest='report_mem_avail', action='store_true')
-    ap.add_argument('--swap-util', dest='report_swap_util', action='store_true')
-    ap.add_argument('--swap-used', dest='report_swap_used', action='store_true')
-    ap.add_argument('--disk-path', dest='mount_path', nargs='+')
-    ap.add_argument('--disk-space-util', dest='report_disk_util', action='store_true')
-    ap.add_argument('--disk-space-used', dest='report_disk_used', action='store_true')
-    ap.add_argument('--disk-space-avail', dest='report_disk_avail', action='store_true')
+    ap.add_argument(
+        '--mem-util', dest='report_mem_util', action='store_true',
+        help='Reports memory utilization in percentages.'
+    )
+    ap.add_argument(
+        '--mem-used', dest='report_mem_used', action='store_true',
+        help='Reports memory used in megabytes.'
+    )
+    ap.add_argument(
+        '--mem-avail', dest='report_mem_avail', action='store_true',
+        help='Reports available memory in megabytes.'
+    )
+    ap.add_argument(
+        '--swap-util', dest='report_swap_util', action='store_true',
+        help='Reports swap utilization in percentages.'
+    )
+    ap.add_argument(
+        '--swap-used', dest='report_swap_used', action='store_true',
+        help='Reports allocated swap space in megabytes.'
+    )
+    ap.add_argument(
+        '--disk-path', dest='mount_path', nargs='+',
+        help='Selects the disk by the path on which to report.'
+    )
+    ap.add_argument(
+        '--disk-space-util', dest='report_disk_util', action='store_true',
+        help='Reports disk space utilization in percentages.'
+    )
+    ap.add_argument(
+        '--disk-space-used', dest='report_disk_used', action='store_true',
+        help='Reports allocated disk space in gigabytes.'
+    )
+    ap.add_argument(
+        '--disk-space-avail', dest='report_disk_avail', action='store_true',
+        help='Reports available disk space in gigabytes.'
+    )
     #ap.add_argument('--auto-scaling:s', dest='auto_scaling')
     #ap.add_argument('--aggregated:s', dest='aggregated')
-    ap.add_argument('--memory-units', dest='mem_units', choices=UNITS.keys(), default='Megabytes')
-    ap.add_argument('--disk-space-units', dest='disk_units', choices=UNITS.keys(), default='Gigabytes')
-    ap.add_argument('--mem-used-incl-cache-buff', dest='mem_used_incl_cache_buff', action='store_true')
-    ap.add_argument('--verify', dest='verify', action='store_true')
-    ap.add_argument('--from-cron', dest='from_cron', action='store_true')
-    ap.add_argument('--verbose', dest='verbose', action='store_true')
+    ap.add_argument(
+        '--memory-units', dest='mem_units', choices=UNITS.keys(), default='Megabytes',
+        help='Specifies units for memory metrics.'
+    )
+    ap.add_argument(
+        '--disk-space-units', dest='disk_units', choices=UNITS.keys(), default='Gigabytes',
+        help='Specifies units for disk space metrics.'
+    )
+    ap.add_argument(
+        '--mem-used-incl-cache-buff', dest='mem_used_incl_cache_buff', action='store_true',
+        help='Count memory that is cached and in buffers as used.'
+    )
+    ap.add_argument(
+        '--verify', dest='verify', action='store_true',
+        help='Checks configuration and prepares a remote call.'
+    )
+    ap.add_argument(
+        '--from-cron', dest='from_cron', action='store_true',
+        help='Specifies that this script is running from cron.'
+    )
+    #ap.add_argument('--verbose', dest='verbose', action='store_true')
     #ap.add_argument('--aws-credential-file:s', dest='aws_credential_file', action='store_true')
     #ap.add_argument('--aws-access-key-id:s', dest='aws_access_key_id', action='store_true')
     #ap.add_argument('--aws-secret-key:s', dest='aws_secret_key', action='store_true')
-    ap.add_argument('--enable-compression', dest='enable_compression', action='store_true')
+    #ap.add_argument('--enable-compression', dest='enable_compression', action='store_true')
     #ap.add_argument('--aws-iam-role:s', dest='aws_iam_role', action='store_true')
 
     args = ap.parse_args()
@@ -288,7 +293,7 @@ def main():
     if args.mount_path and not report_disk:
         raise SystemExit('Metrics to report disk space are provided but disk path is not specified.')
     if not (report_mem or report_disk):
-        raise SystemExit('No metrics specified for collection and submission to CloudWatch.')
+        raise SystemExit('No metrics specified for collection and submission to CloudWatch. Try --help.')
 
     if report_mem:
         collect_memory_and_swap_metrics(args)
